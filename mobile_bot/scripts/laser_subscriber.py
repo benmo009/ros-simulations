da3bff75a48564a27af1b11325c4dd0cb898b53d
Ben <benmo009@gmail.com>
2021-02-15 22:46:18 -0500

    Added proximity detection

--- mobile_bot/scripts/laser_subscriber.py
+++ mobile_bot/scripts/laser_subscriber.py
@@ -6,18 +6,45 @@ import numpy as np
 import matplotlib.pyplot as plt
 import math
 
+class LaserSubscriber:
+    def __init__(self, name):
+        rospy.init_node(name)
+        self.sub = rospy.Subscriber("/mobile_bot/laser/scan", LaserScan, self.laser_callback, queue_size=1)
 
-def radian_to_degree(angle):
-    return 180*angle/math.pi
+    
+    def radian_to_degree(self, angle):
+        return 180*angle/math.pi
 
-def laser_callback(data):
-    print(radian_to_degree(data.angle_min))
+    def degree_to_radian(self, angle):
+        return math.pi*angle/180
+
+    def laser_callback(self, data):
+        angle_min = data.angle_min
+        angle_max = data.angle_max
+        angle_increment = data.angle_increment
+
+        angles = np.arange(angle_min, angle_max, angle_increment)
+
+        laser_range = self.degree_to_radian(120)
+
+        idx_upper = np.argwhere(angles > laser_range)
+        idx_lower = np.argwhere(angles < -laser_range)
+        idx = np.concatenate( (idx_lower, idx_upper), axis=0)
+
+        ranges = np.array(data.ranges)
+
+        distances = ranges[idx]
+
+        proximity = np.argwhere(distances <= 0.2)
+
+        if proximity.size > 0:
+            print("\nTOO CLOSE!!\n")
+
 
 
 if __name__ == "__main__":
-    try:
-        rospy.init_node("Laser_Subscriber")
-        sub = rospy.Subscriber("/mobile_bot/laser/scan", LaserScan, laser_callback, queue_size=1)
+    try:       
+        laser_sub = LaserSubscriber("Laser_Subscriber")
         rospy.spin()
     except rospy.ROSInterruptException:
         pass

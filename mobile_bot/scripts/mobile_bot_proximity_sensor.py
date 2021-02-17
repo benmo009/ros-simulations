70cd09177d7a064c7f157046ba18be8f9b779d5e
Ben <benmo009@gmail.com>
2021-02-17 02:32:25 -0500

    Added proximity sensor node script

---
+++ mobile_bot/scripts/mobile_bot_proximity_sensor.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python
+
+import rospy
+from mobile_bot import ProximitySensor
+
+if __name__ == "__main__":
+    try:
+        proximity_sensor = ProximitySensor.ProximitySensor("mobile_bot_proximity_sensor")
+        rospy.spin()
+    except rospy.ROSInterruptException:
+        pass

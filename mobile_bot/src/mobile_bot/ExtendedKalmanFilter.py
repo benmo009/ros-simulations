bad1375d4e2c33de51ff732459c235b56168e445
benmo009 <benmo009@gmail.com>
2021-02-18 20:53:48 -0500

    Shortened variable names

--- mobile_bot/src/mobile_bot/ExtendedKalmanFilter.py
+++ mobile_bot/src/mobile_bot/ExtendedKalmanFilter.py
@@ -1,21 +1,16 @@
-# Initialization
-#   Get initial q from gazebo service
-#   Add some noise to get initial q_hat and initial P
+# Kalman Filter implementation for mobile robot
 
-
-
-
-import rospy
-import numpy as np
 import math
+import numpy as np
 
 class ExtendedKalmanFilter:
+    # Extended Kalman Filter Constructor
     def __init__(self, A, B, C, f, h, V, W, P0, q0):
         # Functions needed for Kalman Filter
         self.A = A  # Function that takes inputs (q_hat, u), returns Ak matrix
         self.B = B  # Function that takes input q_hat and returns Bk matrix
         self.C = C  # Function that takes input q_hat and returns Ck matrix
-        self.f = f  # Function that takes inputs (q_hat, u), returns open state q_bar
+        self.f = f  # Function that takes inputs (q_hat, u), returns q_bar
         self.h = h  # Function that takes input q_bar, returns predicted output
 
         # Variables needed for Kalman Filter
@@ -32,13 +27,13 @@ class ExtendedKalmanFilter:
         self.q_bar = self.f(self.q_hat, u)
 
         # Compute linearized matrices for the current step
-        A_matrix = self.A(self.q_hat, u)
-        B_matrix = self.B(self.q_hat)
-        C_matrix = self.C(self.q_hat)
+        A_mat = self.A(self.q_hat, u)
+        B_mat = self.B(self.q_hat)
+        C_mat = self.C(self.q_hat)
 
-        A_transpose = np.transpose(A_matrix)
-        B_transpose = np.transpose(B_matrix)
-        C_transpose = np.transpose(C_matrix)
+        A_tran = np.transpose(A_mat)
+        B_tran = np.transpose(B_mat)
+        C_tran = np.transpose(C_mat)
 
         if np.linalg.det(self.V) != 0:
             V_inv = np.linalg.inv(self.V)
@@ -46,9 +41,10 @@ class ExtendedKalmanFilter:
             V_inv = np.zeros((1,1))
 
         # Compute K
-        self.Sigma = A_matrix @ self.P @ A_transpose + B_matrix @ self.W @ B_transpose
-        self.P = np.linalg.inv( np.linalg.inv(self.Sigma) + C_transpose @ V_inv @ C_matrix )
-        self.K = self.P @ C_transpose @ V_inv
+        self.Sigma = A_mat @ self.P @ A_tran + B_mat @ self.W @ B_tran
+        S_inv = np.linalg.inv(self.Sigma)
+        self.P = np.linalg.inv( S_inv + C_tran @ V_inv @ C_mat )
+        self.K = self.P @ C_tran @ V_inv
 
         # Compute next q_hat
         self.q_hat = self.q_bar + self.K * (y - self.h(self.q_bar))

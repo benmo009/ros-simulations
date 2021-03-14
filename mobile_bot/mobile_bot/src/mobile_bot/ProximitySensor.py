3e100729b32674d653dd703b1345b36880bf6f1c
Ben Mo <benmo009@gmail.com>
2021-03-13 19:28:19 -0500

    Edited Proximity sensor

--- mobile_bot/mobile_bot/src/mobile_bot/ProximitySensor.py
+++ mobile_bot/mobile_bot/src/mobile_bot/ProximitySensor.py
@@ -1,3 +1,5 @@
+#!/usr/bin/env python3
+
 import rospy
 from sensor_msgs.msg import LaserScan
 from std_msgs.msg import Bool
@@ -15,7 +17,7 @@ class ProximitySensor:
         # Start laser subscriber
         self.sub = rospy.Subscriber("/mobile_bot/laser/scan", LaserScan, self.laser_callback, queue_size=1)
         # Start publisher
-        self.pub = rospy.Publisher("/mobile_bot/proximity_sensor", Bool, queue_size=1)
+        self.pub = rospy.Publisher("/mobile_bot/proximity_sensor", LaserScan, queue_size=1)
 
         # Stop flag
         self.stop = Bool()
@@ -35,34 +37,48 @@ class ProximitySensor:
         angle_max = data.angle_max
         angle_increment = data.angle_increment
 
-        angles = np.arange(angle_min, angle_max, angle_increment)
-
-        # Range of laser angles to check for proximity
-        laser_range = self.degree_to_radian(120)
-
-        # Check if the lasers point forwards are too close to an object
-        idx_upper = np.argwhere(angles > laser_range)
-        idx_lower = np.argwhere(angles < -laser_range)
-        idx = np.concatenate( (idx_lower, idx_upper), axis=0)
-
         ranges = np.array(data.ranges)
 
-        distances = ranges[idx]
+        angles = np.arange(angle_min, angle_max, angle_increment)
 
-        proximity = np.argwhere(distances <= self.tolerance)
+        cone_ang = 20
+        angle_widths = np.zeros((4,2))
+        angle_widths[0,:] = [0 - cone_ang/2, 0 + cone_ang/2]
+        angle_widths[1,:] = [90 - cone_ang/2, 90 + cone_ang/2]
+        angle_widths[2,:] = [-90 - cone_ang/2, -90 + cone_ang/2]
+        angle_widths[3,:] = [180 - cone_ang/2, -180 + cone_ang/2]
+        angle_widths = self.degree_to_radian(angle_widths)
 
-        # Set stop flag 
-        if proximity.size > 0:
-            print("\nTOO CLOSE!!\n")
-            self.stop.data = True
-        else:
-            print("You're good, keep going")
-            self.stop.data = False
+        sensor_ranges = np.zeros((4,))
 
-        self.pub.publish(self.stop)
 
-    def check_stop(self):
-        return self.stop
+        # Check front of robot
+        idx = np.argwhere(abs(angles) >= angle_widths[3,0])
+        sensor_ranges[0] = np.mean(ranges[idx])
+
+        # Check left of robot
+        idx_lower = np.argwhere(angles >= angle_widths[2,0])
+        idx_upper = np.argwhere(angles <= angle_widths[2,1])
+        idx = np.intersect1d(idx_lower, idx_upper)
+        sensor_ranges[1] = np.mean(ranges[idx])
+
+#        Check back of robot
+        idx = np.argwhere(abs(angles) <= angle_widths[0,1])
+        sensor_ranges[2] = np.mean(ranges[idx])
+        
+        # Check right of robot
+        idx_lower = np.argwhere(angles >= angle_widths[1,0])
+        idx_upper = np.argwhere(angles <= angle_widths[1,1])
+        idx = np.intersect1d(idx_lower, idx_upper)
+        sensor_ranges[3] = np.mean(ranges[idx])
+
+        pub_msg = LaserScan()
+        pub_msg.angle_increment = math.pi / 2
+        pub_msg.angle_min = -math.pi/2
+        pub_msg.angle_max = math.pi
+        pub_msg.ranges = sensor_ranges
+    
+        self.pub.publish(pub_msg)
 
 
 

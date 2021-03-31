9c71f08061a4c0f26ddeb0d4cab0bc13e2fbca59
Ben Mo <benmo009@gmail.com>
2021-03-30 21:38:36 -0400

    Implemented formation control

---
+++ omni_bot/omni_bot/src/FormationControl.py
@@ -0,0 +1,157 @@
+#!/usr/bin/env python3
+
+import rospy 
+from geometry_msgs.msg import Twist, Pose
+import numpy as np
+import networkx as nx
+from networkx.linalg.graphmatrix import incidence_matrix
+
+class FormationControl:
+    def __init__(self, form_rad, rate=20, Kp=0.3, Kp1=0.2, qrange=0.8):
+        self.form_rad = form_rad
+
+        # Initialize ros node
+        rospy.init_node("formation_control")
+
+        self.rate = rospy.Rate(rate)
+        self.dt = 1/rate
+
+        # Get the initial positions of each robot from parameter server
+        while not rospy.has_param('omni_bot/origins'):
+            continue
+
+        self.q = rospy.get_param('omni_bot/origins')
+        self.q = np.array(self.q)
+        (self.N, self.num_states) = self.q.shape
+        self.flat_dim = self.N * self.num_states
+        self.q.shape = (self.flat_dim, )
+
+        # Initialize gains and parameters
+        self.Kp = Kp * np.eye(self.flat_dim)
+        self.Kp1 = Kp1 * np.eye(self.flat_dim)
+        self.qrange = qrange
+
+        rospy.loginfo("Formation Control - Got origins! There are %d robots" % self.N)
+
+        # Set up publisher to each /omni_bot_#/cmd_vel topic
+        self.omni_pubs = []
+        for n in range(self.N):
+            topic = ( "/omni_bot_%d/cmd_vel" % n )
+            self.omni_pubs.append( rospy.Publisher(topic, Twist, queue_size=0))
+
+        # Set up graphs
+        self.InitGraphs()
+
+        # Move to formation
+        self.MoveToFormation(200)
+
+        rospy.loginfo("Formation Control - In Formation!")
+
+        # Start taking keyboard prompts - set up /cmd_vel subscriber
+
+
+    def PublishToOmnibots(self, vel):
+        for n in range(self.N):
+            twist_msg = Twist()
+            twist_msg.linear.x = vel[n,0]
+            twist_msg.linear.y = vel[n,1]
+
+            self.omni_pubs[n].publish(twist_msg)
+
+    
+    # Initializes the information graphs
+    def InitGraphs(self):
+        # Initialize links
+        n1 = list(range(0, self.N))
+        n2 = list(range(1, self.N))
+        n2.append(0)
+
+        # Initialize graphs
+        self.G = nx.Graph()
+        self.G_full = nx.Graph()
+
+        # Construct the graph links
+        n1_full = np.array([])
+        for i in range(self.N):
+            self.G.add_edge(n1[i], n2[i])
+
+            full_edges = np.zeros((self.N-1-i, 2))
+            full_edges[:,0] = i
+            full_edges[:,1] = n1[i+1::]
+        
+            self.G_full.add_edges_from(full_edges)
+
+        # Get the incidence matrices
+        self.D = incidence_matrix(self.G, oriented=True)
+        self.D = self.D.toarray()
+
+        self.D_full = incidence_matrix(self.G_full, oriented=True)
+        self.D_full = self.D_full.toarray()
+        
+        # Number of links
+        (_, self.links) = self.D.shape
+        (_, self.links_full) = self.D_full.shape
+
+        # Kronecker Products
+        self.K = np.kron( np.transpose(self.D), np.eye(self.num_states) )
+        self.K_full = np.kron( np.transpose(self.D_full), np.eye(self.num_states) )
+
+
+    def PositionPotential(self, z_des):
+        z = self.K @ self.q 
+        return z - z_des # Should I make this a member variable????
+
+
+    def PhiFunc(self, x, dx):
+        return (x < dx) * ( (1/x) + (1/dx) )
+
+    def CollisionPotential(self):
+        z_full = self.K_full @ self.q
+        z_flat = np.reshape(z_full, (self.links_full, self.num_states))
+        norm_z = np.linalg.norm(z_flat, axis=-1)
+        norm_z = np.reshape( norm_z, (self.links_full, 1) )
+
+        phi = z_flat * self.PhiFunc(norm_z, self.qrange) / norm_z
+        phi = np.reshape( phi, (self.num_states*self.links_full, ) )
+
+        return phi
+
+    def MoveToFormation(self, T):
+        # Compute desired positions
+        theta_res = 2*np.pi / self.N
+        theta = np.linspace(0, 2*np.pi - theta_res, self.N)
+        q_des = np.zeros((self.N, self.num_states))
+        q_des[:,0] = np.cos(theta)
+        q_des[:,1] = np.sin(theta)
+        q_des *= self.form_rad
+        q_des.shape = (self.flat_dim, )
+
+        # Compute desired z difference variable
+        z_des  = np.kron( np.transpose(self.D), np.eye(self.num_states) ) @ q_des
+
+        for t in range(T):
+            psi = self.PositionPotential(z_des)
+            phi = self.CollisionPotential()
+
+            u = -self.Kp @ np.kron( self.D, np.eye(self.num_states) ) @ psi 
+            u += self.Kp1 @ np.kron(self.D_full, np.eye(self.num_states)) @ phi
+
+            self.q = self.q + self.dt*u
+
+            self.PublishToOmnibots( np.reshape(u, (self.N, self.num_states)) )
+            
+            self.rate.sleep()
+
+        z_final = self.K @ self.q 
+        rospy.loginfo("Final Relative Error")
+        rospy.loginfo(z_final)
+        
+
+
+    def stop(self):
+        for n in range(self.N):
+            twist_msg = Twist()
+            self.omni_pubs[n].publish(twist_msg)
+
+if __name__ == "__main__":
+    fc = FormationControl(2)
---
+++ omni_bot/omni_bot/src/Graph.py
@@ -0,0 +1,341 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from numpy.random import normal
+import networkx as nx 
+import scipy
+import math
+import matplotlib.animation as am
+import pickle
+
+def plot_points(origins):
+    plt.close('all')
+    fig, ax = plt.subplots(1)
+
+    (N, _) = origins.shape
+
+    for i in range(N):
+        ax.scatter(origins[i,0], origins[i,1], c='black')
+        ax.add_patch(plt.Circle(origins[i,:], radius=0.2, 
+                                fc='red', ec='black', alpha=0.5))
+
+    # Set axis limits
+    x_max = np.max( origins[:,0] ) + 0.2
+    x_min = np.min( origins[:,0] ) - 0.2
+    y_max = np.max( origins[:,1] ) + 0.2
+    y_min = np.min( origins[:,1] ) - 0.2
+
+    x_dist = x_max - x_min
+    y_dist = y_max - y_min
+
+    if x_dist > y_dist:
+        y_max += (x_dist - y_dist) / 2
+        y_min -= (x_dist - y_dist) / 2
+    else:
+        x_max += (y_dist - x_dist) / 2
+        x_min -= (y_dist - x_dist) / 2
+    
+    x_lim = 1.1 * np.array([x_min, x_max])
+    y_lim = 1.1 * np.array([y_min, y_max])
+
+    ax.set_xlim( x_lim ) 
+    ax.set_ylim( y_lim )
+
+    ax.set_aspect('equal', adjustable='box')
+
+    plt.show(block=False)
+
+# Generates random origins for each robot
+def generate_origins(N, variance):
+    origins = normal(0, variance, (N, 2))
+    plot_points(origins)
+
+    # Check for overlap
+    reroll = input("Is there overlap? (y/n) ")
+    if reroll.lower() == 'y' or reroll.lower() == 'yes':
+        print("Need to reroll")
+        origins = generate_origins(N, variance)
+
+    return origins
+
+# Potential function
+def s(x, d):
+    return np.log(x / d)
+
+def psifun(x, dx):
+    return (x < dx) * ( (1 / x) + (1 / dx) )
+    
+
+class Animator:
+    def __init__(self, points, rad, xlim, ylim):
+        self.points = points
+        self.rad = rad
+
+        (self.N, self.T) = points.shape
+        self.N = self.N // 2
+
+        self.q0 = np.reshape(points[:,0], (N, 2))
+        self.qf = np.reshape(points[:,-1], (N, 2))
+
+        self.xlim = xlim
+        self.ylim = ylim
+
+
+    def AnimatePoints(self):
+        fig, ax = plt.subplots()
+        ax.set_aspect('equal', adjustable='box')
+
+        self.scat = ax.scatter(self.q0[:,0], self.q0[:,1], c='black')
+        self.circles = []
+        for i in range(self.N):
+            circ = ax.add_patch( plt.Circle(self.q0[i], radius=self.rad, 
+                                            fc='red', ec='black', alpha=0.5) )
+            
+            self.circles.append(circ)
+
+            ax.add_patch( plt.Circle(self.qf[i], radius=self.rad, 
+                                            fc='red', ec='black', alpha=0) )
+    
+    
+        ax.set(xlim=self.xlim, ylim=self.ylim)
+
+        animation = am.FuncAnimation(fig, self.animate, frames=self.T)
+
+        animation.save("simulation.gif", writer='imagemagick', fps=30)
+        print("Done!")
+
+        plt.show()
+
+    def animate(self, i):
+        q = np.reshape(self.points[:,i], (self.N, 2))
+        
+        self.scat.set_offsets(q)
+        for j in range(self.N):
+            self.circles[j].set_center(q[j])
+
+        return self.scat, self.circles
+       
+
+
+
+if __name__ == "__main__":
+    # Number of robots
+    N = 5
+
+    # Generate origins
+    origins = generate_origins(N, 1)
+
+    # Initialize graph
+    n1 = list(range(0, N))
+    n2 = list(range(1, N))
+    n2.append(0)
+
+    # Construct the graph
+    G = nx.Graph()
+    G_full = nx.Graph()
+
+    n1_full = np.array([])
+    for i in range(N):
+        G.add_edge(n1[i], n2[i])
+
+        full_edges = np.zeros((N-1-i, 2))
+        full_edges[:,0] = i
+        full_edges[:,1] = n1[i+1::]
+    
+        G_full.add_edges_from(full_edges)
+
+    D = nx.linalg.graphmatrix.incidence_matrix(G, oriented=True)
+    D = D.toarray()
+
+    D_full = nx.linalg.graphmatrix.incidence_matrix(G_full, oriented=True)
+    D_full = D_full.toarray()
+    
+    # Number of links
+    (_, links) = D.shape
+    (_, links_full) = D_full.shape
+
+    # Number of states for each agent
+    n = 2
+    # Number of time steps
+    steps = 360
+    
+    q = np.zeros((n*N, steps+1))
+    u = np.zeros((n*N, steps))
+
+    # Initial Condition
+    # get the initial robot poses
+    q0 = np.copy(origins)
+    q0.shape = (n*N,)
+
+    q[:,0] = q0
+
+    # Desired distance from center
+    r_des = 2
+
+    # Desired relative posiion and distance for formation
+    theta_res = 2*math.pi / N
+    theta = np.linspace(0, 2*math.pi - theta_res, N)
+
+    q_des = np.zeros((N, 2))
+    q_des[:,0] = np.cos(theta)
+    q_des[:,1] = np.sin(theta)
+    q_des *= r_des
+    q_des.shape = (n*N,)
+
+    z_des = np.kron( np.transpose(D), np.eye(n) ) @ q_des
+    print(z_des)
+    zdis_des = np.linalg.norm( np.reshape(z_des, (links, n)), axis=-1)
+    
+    # Time
+    ts = 0.05
+    t = ts * np.arange(0, steps)
+    
+    # Gains 
+    Kp = 1 * np.eye(n*N)  # 0.3
+    Kp1 = 0.2 * np.eye(n*N)  # 0.2  0.5  0.2
+
+    for k in range(steps):
+
+        zk = np.kron( np.transpose(D), np.eye(n,n) ) @ q[:,k]
+
+        psi = zk - z_des
+        qrange = 0.5 # to avoid collision  2.5  5  0.5
+
+        # Collision Avoidance
+        zkfull = np.kron( np.transpose(D_full), np.eye(n,n) ) @ q[:,k]
+        zkflat = np.reshape(zkfull, (links_full, n))
+        distzk = np.linalg.norm(zkflat, axis=-1)
+        distzk.shape = (links_full, 1)
+
+        phiflat = zkflat * psifun(distzk, qrange) / distzk
+        phi = np.reshape(phiflat, n*links_full,)
+
+        u[:,k] = -Kp@np.kron( D, np.eye(n) ) @ psi + Kp1 @ np.kron(D_full, np.eye(n)) @ phi
+        
+        q[:,k+1] = q[:,k] + ts * u[:,k]
+
+
+    zf = np.kron( np.transpose(D), np.eye(n) ) @ q[:,-1]
+    print("Final Relative Error")
+    print(np.reshape(zf - z_des, (links, n)))
+
+    fig, ax = plt.subplots()
+    scat = ax.scatter(origins[:,0], origins[:,1], c='red', label="q0")
+
+    qf = np.reshape(q[:,-1], (N,2))
+    ax.scatter(qf[:,0], qf[:,1], c='green', label="qf")
+
+    circles = []
+    for i in range(N):
+        circ = ax.add_patch(plt.Circle((origins[i,0],origins[i,1]), radius=0.2, 
+                                    fc='red', ec='black', alpha=0.5))
+
+        ax.add_patch(plt.Circle((qf[i,0],qf[i,1]), radius=0.2, 
+                                    fc='green', ec='black', alpha=0.5))
+
+
+    
+
+    # ax.add_patch(plt.Circle((qf[:,0],qf[:,1]), radius=0.2, 
+    #                                 fc='red', ec='black', alpha=0.5))
+
+
+
+    for i in range(N):
+        plt.plot(q[i*n, :], q[i*n+1, :], c='blue')
+    
+    plt.legend()
+
+    ax.set_aspect('equal', adjustable='box')
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+
+    plt.show()
+    
+    animator = Animator(q, 0.2, xlim, ylim)
+    animator.AnimatePoints()
+
+    # ===================================================================================
+    # Rotate the circle
+
+    ang_vel = 1  # 1 rad / s
+    th = ang_vel * ts
+
+    R = np.array([ [np.cos(th), -np.sin(th)],
+                    [np.sin(th), np.cos(th)] ])
+
+    q0 = np.reshape(q[:,-1], (N,n))
+    q[:,0] = q[:,-1]
+        
+    # Time
+    ts = 0.05
+    t = ts * np.arange(0, steps)
+
+    for k in range(steps):
+        current_q = np.reshape(q[:,k], (N,n))
+        q_des = current_q @ np.transpose(R)
+        q_des.shape = (n*N,)
+        z_des = np.kron( np.transpose(D), np.eye(n) ) @ q_des
+
+        zk = np.kron( np.transpose(D), np.eye(n,n) ) @ q[:,k]
+
+        psi = zk - z_des
+        qrange = 0.8 # to avoid collision  2.5  5  0.5
+
+        # Collision Avoidance
+        zkfull = np.kron( np.transpose(D_full), np.eye(n,n) ) @ q[:,k]
+        zkflat = np.reshape(zkfull, (links_full, n))
+        distzk = np.linalg.norm(zkflat, axis=-1)
+        distzk.shape = (links_full, 1)
+
+        phiflat = zkflat * psifun(distzk, qrange) / distzk
+        phi = np.reshape(phiflat, n*links_full,)
+
+        u[:,k] = -Kp @ np.kron( D, np.eye(n) ) @ psi + Kp1 @ np.kron(D_full, np.eye(n)) @ phi
+        
+        q[:,k+1] = q[:,k] + ts * u[:,k]
+
+
+    zf = np.kron( np.transpose(D), np.eye(n) ) @ q[:,-1]
+    print("Final Relative Error")
+    print(np.reshape(zf - z_des, (links, n)))
+
+    fig, ax = plt.subplots()
+    origins = np.reshape(q[:,0], (N,2))
+    scat = ax.scatter(origins[:,0], origins[:,1], c='red', label="q0")
+
+    qf = np.reshape(q[:,-1], (N,2))
+    ax.scatter(qf[:,0], qf[:,1], c='green', label="qf")
+
+    circles = []
+    qf = np.reshape(q[:,-1], (N,2))
+    for i in range(N):
+        circ = ax.add_patch(plt.Circle((origins[i,0],origins[i,1]), radius=0.2, 
+                                    fc='red', ec='black', alpha=0.5))
+
+        ax.add_patch(plt.Circle((qf[i,0],qf[i,1]), radius=0.2, 
+                                    fc='green', ec='black', alpha=0.5))
+
+
+    
+
+    # ax.add_patch(plt.Circle((qf[:,0],qf[:,1]), radius=0.2, 
+    #                                 fc='red', ec='black', alpha=0.5))
+
+
+
+    for i in range(N):
+        plt.plot(q[i*n, :], q[i*n+1, :], c='blue')
+    
+    plt.legend()
+
+    ax.set_aspect('equal', adjustable='box')
+    xlim = ax.get_xlim()
+    ylim = ax.get_ylim()
+
+    plt.show()
+    
+    animator = Animator(q, 0.2, xlim, ylim)
+    animator.AnimatePoints()
+
+    
+    
